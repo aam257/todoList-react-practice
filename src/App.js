127595974ec5dd8ab251706785daac8b1d6f7a12
Aniket <aam257@njit.edu>
2021-01-31 16:55:55 -0500

    Todo component with add delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 // Components import
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>to do list...</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Todo from "./Todo";
 
-export const TodoList = () => {
+const TodoList = ({ todos, setTodos }) => {
+  //console.log(todos);
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            todo={todo}
+            text={todo.text}
+            key={todo.id}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
+
+export default TodoList;
